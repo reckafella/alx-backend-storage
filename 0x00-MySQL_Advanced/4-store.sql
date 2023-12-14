8a98dbc81620d4c7ca75cf874ec315ff6982ccd9
reckafella <ethanmuthoniw6@gmail.com>
2023-12-14 18:18:09 +0300

    add task 4

---
+++ 0x00-MySQL_Advanced/4-store.sql
@@ -0,0 +1,17 @@
+ -- a SQL script that creates a trigger that decreases the quantity of an item after adding a new order.
+
+-- Quantity in the table items can be negative
+
+DELIMITER $$ ; -- change delimiter so semicolon can be included in trigger body
+
+CREATE TRIGGER `decrease_quantity`
+AFTER INSERT ON `orders`
+FOR EACH ROW
+BEGIN
+    UPDATE `items`
+    SET `quantity` = `quantity` - NEW.`number`
+    WHERE `name` = NEW.`item_name`;
+END;
+$$
+
+DELIMITER ; $$ -- reset delimiter
