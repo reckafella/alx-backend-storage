bd8507e0e60d40cf6d8b37cceb03b3a3d28eb5a5
reckafella <ethanmuthoniw6@gmail.com>
2023-12-14 17:25:42 +0300

    modify file

--- 0x00-MySQL_Advanced/3-glam_rock.sql
+++ 0x00-MySQL_Advanced/3-glam_rock.sql
@@ -8,5 +8,5 @@
 
 SELECT `band_name`, IFNULL(`split` - `formed`, 2022 - `formed`) as `lifespan`
 FROM `metal_bands`
-WHERE  FIND_IN_SET('Glam rock', style) > 0
+WHERE  FIND_IN_SET('Glam rock', IFNULL(style, '')) > 0
 ORDER BY `lifespan`
