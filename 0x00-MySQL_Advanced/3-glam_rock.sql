7ccc970c4e5f0483077fcc18c1cdb32f2bae6c88
reckafella <ethanmuthoniw6@gmail.com>
2023-12-14 17:26:05 +0300

    add delimiter

--- 0x00-MySQL_Advanced/3-glam_rock.sql
+++ 0x00-MySQL_Advanced/3-glam_rock.sql
@@ -9,4 +9,4 @@
 SELECT `band_name`, IFNULL(`split` - `formed`, 2022 - `formed`) as `lifespan`
 FROM `metal_bands`
 WHERE  FIND_IN_SET('Glam rock', IFNULL(style, '')) > 0
-ORDER BY `lifespan`
+ORDER BY `lifespan`;
