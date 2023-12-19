5152864020be4c969c34d0e8d611fa336b84a4b4
Reckafella <ethanmuthoni@gmail.com>
2023-12-19 22:35:21 +0300

    add 8-all.py

---
+++ 0x01-NoSQL/8-all.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+'''
+Module contains a Python function that lists all documents in a collection
+'''
+import pymongo
+
+
+def list_all(mongo_collection):
+    '''
+    Python function that lists all documents in a collection
+    '''
+    documents = []
+    result = mongo_collection.find()
+    for document in result:
+        documents.append(document)
+    return documents
