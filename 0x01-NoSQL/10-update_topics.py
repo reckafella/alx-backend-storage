0b8cebb67143c42a6042fdf65ddffdebdacf04c6
Reckafella <ethanmuthoni@gmail.com>
2023-12-20 00:08:01 +0300

    update task 10

--- 0x01-NoSQL/10-update_topics.py
+++ 0x01-NoSQL/10-update_topics.py
@@ -8,4 +8,4 @@ def update_topics(mongo_collection, name, topics):
     '''
     changes all topics of a school document based on the name
     '''
-    mongo_collection.update_many({"name": name}, {"$set": {'topics': topics}})
+    mongo_collection.update_many({'name': name}, {'$set': {'topics': topics}})
