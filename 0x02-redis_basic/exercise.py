a83e8c51159738010010b95cf443aa9f1dc4ae1e
Reckafella <ethanmuthoni@gmail.com>
2023-12-20 23:56:38 +0300

    update exercise

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -4,7 +4,7 @@ Module contains a class to store an instance of the Redis client
 '''
 import redis
 import uuid
-from typing import Union
+from typing import Union, Callable
 
 
 class Cache:
@@ -27,3 +27,25 @@ class Cache:
         key: str = str(uuid.uuid4())
         self._redis.set(key, data)
         return key
+
+    def get(self, key: str, fn: Callable) ->\
+            Union[str, bytes, int, float, list]:
+        '''
+        method that take a key string argument and an optional Callable\
+            argument named fn. This callable will be used to convert the\
+                data back to the desired format.
+        '''
+        result = self._redis.get(key)
+        return fn(result) if result else None
+
+    def get_str(self, data: bytes) -> str:
+        '''
+        returns a string representation of data received from redis
+        '''
+        return data.decode('utf-8')
+
+    def get_int(self, data: bytes) -> int:
+        '''
+        return integer representation of data received from redis
+        '''
+        return int(self.get_str(data=data))
