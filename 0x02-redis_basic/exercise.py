975df00dc9c74f5c518caef599e8b48512c65002
reckafella <ethanmuthoniw6@gmail.com>
2023-12-20 20:21:41 +0300

    more updates

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -4,7 +4,7 @@ Module contains a class to store an instance of the Redis client
 '''
 import redis
 import uuid
-
+from typing import Union
 
 class Cache:
     '''
@@ -14,15 +14,15 @@ class Cache:
         '''
         stores an instance of the Redis client in _redis
         '''
-        self.__redis: redis.Redis = redis.Redis()
-        self.__redis.flushdb(asynchronous=True)
+        self._redis: redis.Redis = redis.Redis()
+        self._redis.flushdb(asynchronous=True)
 
-    def store(self, data: str | bytes | int | float) -> str:
+    def store(self, data: Union[str, bytes, int, float]) -> str:
         '''
         takes a data argument and returns a string
         generates a random key using uuid, stores the input data in Redis\
             using the random key and return the key
         '''
-        self.key: uuid.uuid4 = str(uuid.uuid4())
-        self.__redis.set(self.key, data)
-        return self.key
+        key: uuid.uuid4 = str(uuid.uuid4())
+        self._redis.set(key, data)
+        return key
